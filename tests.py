bdfb35210fcd6df97a2210f5ad5ddf25794a6c7a
Byron Machin <bmachin7@gmail.com>
2024-10-06 22:04:57 -0500

    Unitary tests.

---
+++ tests.py
@@ -0,0 +1,106 @@
+import pytest
+from api import API
+
+def test_basic_route_adding(api):
+    @api.route("/home")
+    def home(req, resp):
+        resp.text = "YOLO"
+
+def test_route_overlap_throws_exception(api):
+    @api.route("/home")
+    def home(req, resp):
+        resp.text = "YOLO"
+    
+    with pytest.raises(AssertionError):
+        @api.route("/home")
+        def home2(req, resp):
+            resp.text = "Duplicated route"
+
+def test_client_can_send_requests(api, client):
+    RESPONSE_TEXT = "THIS IS COOL"
+
+    @api.route("/hey")
+    def cool(req, resp):
+        resp.text = RESPONSE_TEXT
+
+    assert client.get("http://testserver/hey").text == RESPONSE_TEXT
+
+
+def test_parametrized_route(api, client):
+    @api.route("/{name}")
+    def hello(req, resp, name):
+        resp.text = f"Hey, {name}!"
+    
+    assert client.get("http://testserver/Byron").text == "Hey, Byron!"
+    assert client.get("http://testserver/David").text == "Hey, David!"
+
+def test_default_404_response(client):
+    response = client.get("http://testserver/doesnotexist")
+
+    assert response.status_code == 404
+    assert response.text == "Not found"
+
+def test_class_based_handler_get(api, client):
+    response_text = "This is a get request"
+
+    @api.route("/book")
+    class BookResource:
+        def get(self, req, resp):
+            resp.text = response_text
+
+    assert client.get("http://testserver/book").text == response_text
+
+def test_class_based_handler_post(api, client):
+    response_text = "This is a post request"
+
+    @api.route("/book")
+    class BookResource:
+        def post(self, req, resp):
+            resp.text = response_text
+        
+    assert client.post("http://testserver/book").text == response_text
+
+def test_class_based_handler_not_allowed_method(api, client):
+    @api.route("/book")
+    class BookResource:
+        def post(self, req, resp):
+            resp.text = "yolo"
+
+    with pytest.raises(AttributeError):
+        client.get("http://testserver/book")
+
+
+def test_alternative_route(api, client):
+    response_text = "Alternative way to add a route"
+
+    def home(req, resp):
+        resp.text = response_text
+
+    api.add_route("/alternative", home)
+
+    assert client.get("http://testserver/alternative").text == response_text
+
+
+def test_template(api, client):
+    @api.route("/html")
+    def html_handler(req, resp):
+        resp.body = api.template("index.html", context={"title": "Some title", "name": "Byron"}).encode()
+    
+    response = client.get("http://testserver/html")
+
+    assert "text/html" in response.headers["Content-Type"]
+    assert "Some title" in response.text
+    assert "Byron" in response.text
+
+
+def test_custon_exception_handler(api, client):
+    def on_exception(req, resp, exc):
+        resp.text = "AttributeErrorHappened"
+    
+    @api.route("/")
+    def index(req, resp):
+        raise AttributeError()
+
+    response = client.get("http://testserver/")
+
+    assert response.text == "AttributeErrorHappened"
