3c22334c52ec21321a283f21ffd4bf78de845569
Byron Machin <bmachin7@gmail.com>
2024-10-06 22:05:59 -0500

    Added sessions for requests and template handlers.

--- api.py
+++ api.py
@@ -1,12 +1,20 @@
+import os
 import inspect
 
 from parse import parse
 from webob import Request, Response
+from requests import Session as RequestsSession
+from wsgiadapter import WSGIAdapter as RequestsWSGIAdapter
+from jinja2 import Environment, FileSystemLoader
 
 
 class API:
-    def __init__(self):
+    def __init__(self, templates_dir="templates"):
         self.routes = {}
+
+        self.templates_env = Environment(
+            loader=FileSystemLoader(os.path.abspath(templates_dir))
+        )
 
     def __call__(self, environ, start_response):
         request = Request(environ)
@@ -16,13 +24,16 @@ class API:
         return response(environ, start_response)
 
     def route(self, path):
-        assert path not in self.routes, f"Path '{path}' already exists."
-        
         def wrapper(handler):
-            self.routes[path] = handler
+            self.add_route(path, handler)
             return handler
+        
         return wrapper
     
+    def add_route(self, path, handler):
+        assert path not in self.routes, f"Path '{path}' already exists."
+
+        self.routes[path] = handler
 
     def find_handler(self, request_path):
         for path, handler in self.routes.items():
@@ -51,3 +62,14 @@ class API:
     def default_response(self, response):
         response.status_code = 404
         response.text = "Not found"
+
+    def test_session(self, base_url="http://testserver"):
+        session = RequestsSession()
+        session.mount(prefix=base_url, adapter=RequestsWSGIAdapter(self))
+        return session
+
+    def template(self, template_name, context=None):
+        if context is None:
+            context = {}
+        
+        return self.templates_env.get_template(template_name).render(**context)
